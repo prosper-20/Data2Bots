0a19c523858d1a2f4f43c0fce032b061d7588142
prosper <edwardprosper001@gmail.com>
2022-09-20 21:42:23 +0100

    Updates

--- core/models.py
+++ core/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"slug":self.slug})
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from .views import HomeView, ItemDetailView, checkout
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("checkout/", checkout, name="checkout"),
-    path("product/<slug:slug>/", ItemDetailView.as_view(), name="products")
+    path("product/<slug:slug>/", ItemDetailView.as_view(), name="product")
 ]
