eee54908f943093b87d4633d228de58c109fd123
prosper <edwardprosper001@gmail.com>
2022-09-20 22:29:31 +0100

    Updates

--- core/models.py
+++ core/models.py
@@ -22,6 +22,7 @@ class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
@@ -33,7 +34,7 @@ class Order(models.Model):
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
 
-    def _str__(self):
+    def __str__(self):
         return self.user.username
 
 
