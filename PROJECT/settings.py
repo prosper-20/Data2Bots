aa516e7fc2a15a9e043cb6281e01a8940c9b5bf3
prosper <edwardprosper001@gmail.com>
2022-09-20 21:24:43 +0100

    Updates

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
