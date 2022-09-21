edcc6e1fd844601c40fabcb795b560370c065549
prosper <edwardprosper001@gmail.com>
2022-09-21 22:49:44 +0100

    improvements

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -49,6 +49,15 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
--- core/api/views.py
+++ core/api/views.py
@@ -3,6 +3,10 @@ from rest_framework.response  import Response
 from .serializers import ItemSerializer
 from rest_framework.decorators import api_view
 from core.models import Item
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(["GET"])
@@ -15,3 +19,12 @@ def api_item_list_view(request):
     if request.method == "GET":
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
+
+
+
+class ItemListView(ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    pagination_class = PageNumberPagination
